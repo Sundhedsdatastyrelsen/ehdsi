635147e2712b25424455baf6d31518bb8e19fb3d
Hans Bugge Grathwohl <hbg@globeteam.com>
2023-11-23 21:18:21 +0100

    Set up Tomcat container with TRC-STS, OpenATNA, and Gateway

--- NCP/openncp-gateway/setenv.sh
+++ NCP/openncp-gateway/setenv.sh
@@ -6,9 +6,8 @@ export CATALINA_BASE=/usr/local/tomcat
 MARIADB_USERNAME="$(cat "${MARIADB_USERNAME_FILE}")"
 MARIADB_PASSWORD="$(cat "${MARIADB_PASSWORD_FILE}")"
 TLS_KEYSTORE_PASSWORD="$(cat "${TLS_KEYSTORE_PASSWORD_FILE}")"
-TLS_TRUSTSTORE_PASSWORD="$(cat "${TLS_TRUSTSTORE_PASSWORD_FILE}")"
 
-# Used in server.xml:
+CATALINA_OPTS="$CATALINA_OPTS -DopenATNA.properties.path=file:$EPSOS_PROPS_PATH/ATNA_resources/openatna.properties"
 CATALINA_OPTS="$CATALINA_OPTS -Ddb.host=\"${MARIADB_HOST}\""
 CATALINA_OPTS="$CATALINA_OPTS -Ddb.port=\"${MARIADB_PORT}\""
 CATALINA_OPTS="$CATALINA_OPTS -Ddb.username=\"${MARIADB_USERNAME}\""
@@ -16,5 +15,6 @@ CATALINA_OPTS="$CATALINA_OPTS -Ddb.password=\"${MARIADB_PASSWORD}\""
 CATALINA_OPTS="$CATALINA_OPTS -Dtls.keystore.alias=\"${TLS_KEYSTORE_ALIAS}\""
 CATALINA_OPTS="$CATALINA_OPTS -Dtls.keystore.file=\"${TLS_KEYSTORE_FILE}\""
 CATALINA_OPTS="$CATALINA_OPTS -Dtls.keystore.password=\"${TLS_KEYSTORE_PASSWORD}\""
-CATALINA_OPTS="$CATALINA_OPTS -Dtls.truststore.file=\"${TLS_TRUSTSTORE_FILE}\""
-CATALINA_OPTS="$CATALINA_OPTS -Dtls.truststore.password=\"${TLS_TRUSTSTORE_PASSWORD}\""
+
+# See: https://webgate.ec.europa.eu/fpfis/wikis/display/EHDSI/OpenNCP+Installation+Manual#OpenNCPInstallationManual-4.1.1OpenNCPGateway
+export spring_config_location="$EPSOS_PROPS_PATH/gateway/application.yml"
