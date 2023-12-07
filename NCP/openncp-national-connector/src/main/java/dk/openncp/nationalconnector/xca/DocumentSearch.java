c1b3d8aa4e3f19315c18e9576c0e0f99912ba649
Hans Bugge Grathwohl <hbg@globeteam.com>
2023-12-07 22:37:30 +0100

    Send all search criteria

--- NCP/openncp-national-connector/src/main/java/dk/openncp/nationalconnector/xca/DocumentSearch.java
+++ NCP/openncp-national-connector/src/main/java/dk/openncp/nationalconnector/xca/DocumentSearch.java
@@ -24,6 +24,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -79,12 +80,26 @@ public class DocumentSearch implements NationalConnectorInterface, DocumentSearc
         return Stream.of("patientId", "classCode", "document")
                 .allMatch(field -> doc.hasNonNull(field) && doc.get(field).isTextual());
     }
+
+    private static String lowercaseFirstLetter(String s) {
+        return Character.toLowerCase(s.charAt(0)) + s.substring(1);
+    }
+
+    private static String queryString(SearchCriteria searchCriteria) {
+        return Arrays.stream(SearchCriteria.Criteria.values())
+                .flatMap(c -> {
+                    var criteriaValue = searchCriteria.getCriteriaValue(c);
+                    if (criteriaValue != null) {
+                        return Stream.of(lowercaseFirstLetter(c.value) + "=" + URLEncoder.encode(criteriaValue, StandardCharsets.UTF_8));
+                    }
+                    return Stream.empty();
+                })
+                .reduce((p1, p2) -> p1 + "&" + p2)
+                .orElse("");
+    }
     private static EPSOSDocument getDocumentFromService(SearchCriteria searchCriteria) throws NIException {
         try {
-            var uri = new URI(countryAHost + "/api/documents/?patientId=" + URLEncoder.encode(
-                    searchCriteria.getPatientId().getExtension() + "^^^" + searchCriteria.getPatientId().getRoot(),
-                    StandardCharsets.UTF_8
-            ));
+            var uri = new URI(countryAHost + "/api/documents/?" + queryString(searchCriteria));
 
             var request = HttpRequest.newBuilder(uri).GET().build();
             var response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
@@ -135,5 +150,9 @@ public class DocumentSearch implements NationalConnectorInterface, DocumentSearc
      * For testing/debugging in IDE
      */
     public static void main(String[] args) throws NIException, InsufficientRightsException {
+        var sc = new SearchCriteriaImpl();
+        sc.addPatientId("DKCPR^^^1234567890");
+        sc.add(SearchCriteria.Criteria.DOCUMENT_ID, "21298478");
+        getDocumentFromService(sc);
     }
 }
