468c16d5fa5cdbfbf664de0ec2a1013a379bd424
Christian Frausing Brams <cfb@globeteam.com>
2024-06-25 12:36:02 +0200

    Remove e-health portal project

--- ehealth-portal/ehealth-portal-backend/src/main/java/eu/europa/ec/sante/openncp/portal/service/AuditService.java
+++
@@ -1,9 +0,0 @@
-package eu.europa.ec.sante.openncp.portal.service;
-
-public class AuditService {
-
-    public void processClinicianIdentificationAudit() {
-
-        //EventLog eventLog = EventLog.createEventLogNOKA();
-    }
-}
