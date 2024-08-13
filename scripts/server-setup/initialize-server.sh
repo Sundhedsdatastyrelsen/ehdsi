f46af5e5d00e11f9032420d9a8a61c6e29a0f74a
Christian Frausing Brams <cfb@globeteam.com>
2024-08-13 13:36:11 +0200

    Create initialize script for server

---
+++ scripts/server-setup/initialize-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Create user for running the system
+sudo adduser epps-server
+
+sudo apt-get update
+
+# Install docker, by adding their repository to apt-get
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo mkdir /var/ehdsi
+sudo chown /var/ehdsi epps-server
+
+read -sp 'API Token for Github login: ' passvar
+export GITHUB_PAT=${passvar}
+
