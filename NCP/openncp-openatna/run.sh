32535b9855a46aba2cae1c15fd25c35aca1bf3a2
Christian Frausing Brams <christian@brams.dk>
2024-10-16 15:40:47 +0200

    Make openATNA use the new secrets

--- NCP/openncp-openatna/run.sh
+++ NCP/openncp-openatna/run.sh
@@ -3,9 +3,10 @@
 # Export secrets as environment variables
 export TLS_KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)
 export TLS_TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
-export DB_USERNAME=$(cat /run/secrets/db_username)
-export DB_PASSWORD=$(cat /run/secrets/db_password)
+export KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)
+export TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export DB_USER=$(cat /run/secrets/db_username)
+export DB_PWD=$(cat /run/secrets/db_password)
 
 # Execute the original command
 exec "$@"
-
