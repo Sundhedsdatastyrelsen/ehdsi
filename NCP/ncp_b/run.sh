14132bcff46a3082b696b68e81e97728d65a9917
Christian Frausing Brams <christian@brams.dk>
2024-10-16 15:39:38 +0200

    Make the ncp_b use the new secrets

--- NCP/ncp_b/run.sh
+++ NCP/ncp_b/run.sh
@@ -3,6 +3,10 @@
 # Export secrets as environment variables
 export TLS_KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)
 export TLS_TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)  #Duplicate since it is unclear which one is used
+export TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export DB_USER=$(cat /run/secrets/db_username)
+export DB_PWD=$(cat /run/secrets/db_password)
 
 # Execute the original entrypoint script
 exec "$@"
