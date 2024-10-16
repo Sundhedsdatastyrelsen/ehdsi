c37fb57a63c84c799d2b2164272731e1373da586
Christian Frausing Brams <christian@brams.dk>
2024-10-16 15:39:13 +0200

    Make the ncp_a use the new secrets

--- NCP/ncp_a/run.sh
+++ NCP/ncp_a/run.sh
@@ -4,8 +4,10 @@ set -o nounset
 set -o pipefail
 
 # Export secrets as environment variables
-export KEYSTORE_PASSWORD=$(cat /run/secrets/keystore_password_file)
-export TRUSTSTORE_PASSWORD=$(cat /run/secrets/truststore_password_file)
+export KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)  
+export TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export DB_USER=$(cat /run/secrets/db_username)
+export DB_PWD=$(cat /run/secrets/db_password)
 
 # Execute the original command
 exec "$@"
