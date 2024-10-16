3b2230b4bd0348a1cd95a31b931ec2f77751e29c
Christian Frausing Brams <christian@brams.dk>
2024-10-16 14:16:37 +0200

    Create country B service

---
+++ NCP/ncp_b/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Export secrets as environment variables
+export TLS_KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)
+export TLS_TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+
+# Execute the original entrypoint script
+exec "$@"
+
