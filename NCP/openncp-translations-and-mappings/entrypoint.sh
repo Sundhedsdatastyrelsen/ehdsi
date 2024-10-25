c0cf5367304350d0611774a9d2e6850eb023ff38
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-10-25 16:07:20 +0200

    translations and mappings service

---
+++ NCP/openncp-translations-and-mappings/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Export secrets as environment variables
+export TLS_KEYSTORE_PASSWORD; TLS_KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)
+export TLS_TRUSTSTORE_PASSWORD; TLS_TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export KEYSTORE_PASSWORD; KEYSTORE_PASSWORD=$(cat /run/secrets/tls_keystore_password)  #Duplicate since it is unclear which one is used
+export TRUSTSTORE_PASSWORD; TRUSTSTORE_PASSWORD=$(cat /run/secrets/tls_truststore_password)
+export DB_USER; DB_USER=$(cat /run/secrets/db_username)
+export DB_PWD; DB_PWD=$(cat /run/secrets/db_password)
+
+envsubst \
+    < /opt/atna-resources/ArrConnections.template.xml \
+    > /opt/atna-resources/ArrConnections.xml
+
+# Execute the original command
+exec "$@"
