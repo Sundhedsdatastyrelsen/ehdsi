ffbda0406c89641bfdf2282744e96f566dcd15b4
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-10-25 15:24:14 +0200

    openatna

--- NCP/openncp-configuration-utility/tomcat-config-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-envsubst < /opt/ehealth-openncp/openncp-configuration-utility/openncp-configuration.template.properties > \
-/opt/ehealth-openncp/openncp-configuration-utility/openncp-configuration.properties
-
-# Run the standard container command.
-exec "$@"
