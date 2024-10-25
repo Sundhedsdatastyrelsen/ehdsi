a2275dc8fc773264df72dd3858dd81bb4c836379
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-10-25 15:37:49 +0200

    fix tsam-exporter

--- NCP/openncp-tsam-exporter/entrypoint.sh
+++ NCP/openncp-tsam-exporter/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -6,3 +8,5 @@ set -o pipefail
 export DB_USER; DB_USER=$(<"${DB_USER_FILE}")
 export DB_PASSWORD; DB_PASSWORD=$(<"${DB_PASSWORD_FILE}")
 
+# Execute the original command
+exec "$@"
