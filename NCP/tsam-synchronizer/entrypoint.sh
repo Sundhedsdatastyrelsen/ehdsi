089670b7c152d0385e27c7809f9330e5af3bb60e
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-11-04 15:22:17 +0100

    Add TSAM synchronizer to new Docker setup

---
+++ NCP/tsam-synchronizer/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+export DB_USER; DB_USER=$(</run/secrets/db_username)
+export DB_PASSWORD; DB_PASSWORD=$(</run/secrets/db_password)
+export TSAM_SYNC_USERNAME; TSAM_SYNC_USERNAME=$(</run/secrets/cts_username)
+export TSAM_SYNC_PASSWORD; TSAM_SYNC_PASSWORD=$(</run/secrets/cts_password)
+
+# Execute the original command
+exec "$@"
