6d638d94a2d0378793fc3c48758d46891f9dced4
Hans Bugge Grathwohl <hbg@globeteam.com>
2024-11-14 14:49:33 +0100

    Fix test-tool for wave 8

--- NCP/test-tool/request_xca_retrieve.sh
+++ NCP/test-tool/request_xca_retrieve.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 "$SCRIPT_DIR/run.sh" \
     -X request-builder/build \
-    :hcp-template     "\"$SCRIPT_DIR/templates/xca/retrieve/hcp.xml\"" \
-    :trc-template     "\"$SCRIPT_DIR/templates/xca/retrieve/trc.xml\"" \
+    :hcp-template     "\"$SCRIPT_DIR/templates/xca/hcp.xml\"" \
+    :trc-template     "\"$SCRIPT_DIR/templates/xca/trc.xml\"" \
     :request-template "\"$SCRIPT_DIR/templates/xca/retrieve/request.xml\"" \
-    :private-key      "\"$SCRIPT_DIR/client-dk.key\"" \
-    :cert             "\"$SCRIPT_DIR/client-dk.crt\""
+    :private-key      "\"$SCRIPT_DIR/testcert.p8.pem\"" \
+    :cert             "\"$SCRIPT_DIR/testcert.cer\""
