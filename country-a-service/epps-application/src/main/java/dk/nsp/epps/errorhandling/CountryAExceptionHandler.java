2f07028d6576a691d874f58f5d861bfc2a8b3956
Christian Frausing Brams <cfb@globeteam.com>
2024-11-28 14:26:52 +0100

    Add error id tracing to handling of Country A exceptions

--- country-a-service/epps-application/src/main/java/dk/nsp/epps/errorhandling/CountryAExceptionHandler.java
+++ country-a-service/epps-application/src/main/java/dk/nsp/epps/errorhandling/CountryAExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.UUID;
+
 @Slf4j
 @Order(1)
 @RestControllerAdvice
@@ -16,17 +18,17 @@ public class CountryAExceptionHandler {
     @ExceptionHandler(CountryAException.class)
     public ResponseEntity<ErrorDto> handleException(CountryAException e) {
         HttpStatus httpStatus = e.getHttpStatus();
-        //CFB: I'm not sure we want to expose our internal error messages to the OpenNCP error. We should probably just
-        // give them some idea that we failed, and a id or timestamp they can give us, and we can look in the code for.
-        var details = new ErrorDto(httpStatus.name(), e.getMessage() != null ? e.getMessage() : e.getClass()
-            .getSimpleName());
+
+        var errorUuid = UUID.randomUUID();
+
+        var details = new ErrorDto(httpStatus.name(), String.format("Error id: %s",errorUuid));
 
         if (httpStatus == HttpStatus.INTERNAL_SERVER_ERROR) {
-            log.error("{}: {} - Returning {}", e.getClass().getSimpleName(), e.getMessage(), httpStatus, e);
+            log.error("{}: {} - (Error id: {}) - Returning {}", e.getClass().getSimpleName(), e.getMessage(), errorUuid, httpStatus, e);
         } else if (httpStatus.is5xxServerError()) {
-            log.warn("{}: {} - Returning {}", e.getClass().getSimpleName(), e.getMessage(), httpStatus);
+            log.warn("{}: {} - (Error id: {}) - Returning {}", e.getClass().getSimpleName(), e.getMessage(), errorUuid, httpStatus);
         } else {
-            log.info("{}: {} - Returning {}", e.getClass().getSimpleName(), e.getMessage(), httpStatus);
+            log.info("{}: {} - (Error id: {}) - Returning {}", e.getClass().getSimpleName(), e.getMessage(), errorUuid, httpStatus);
         }
 
         return ResponseEntity.status(httpStatus).body(details);
