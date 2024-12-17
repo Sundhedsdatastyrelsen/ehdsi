4442cc2b276a95817b2ea616662c27f290d4b479
Christian Frausing Brams <cfb@globeteam.com>
2024-12-17 14:55:09 +0100

    Use karl for testing

--- country-a-service/epps-service/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/service/mapping/EPrescriptionMapperTest.java
+++ country-a-service/epps-service/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/service/mapping/EPrescriptionMapperTest.java
@@ -1,6 +1,7 @@
 package dk.sundhedsdatastyrelsen.ncpeh.service.mapping;
 
 import dk.sundhedsdatastyrelsen.ncpeh.service.PrescriptionService.PrescriptionFilter;
+import dk.sundhedsdatastyrelsen.ncpeh.testing.shared.Fmk;
 import dk.sundhedsdatastyrelsen.ncpeh.testing.shared.FmkResponseStorage;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.EPrescriptionDocumentIdMapper;
 import org.junit.jupiter.api.Assertions;
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class EPrescriptionMapperTest {
     @Test
     public void testExpectedNumberOfEpsosDocuments() throws Exception {
-        var response = FmkResponseStorage.storedPrescriptions("1111111118");
+        var response = FmkResponseStorage.storedPrescriptions(Fmk.cprKarl);
         var documentId = EPrescriptionDocumentIdMapper.level3DocumentId(String.valueOf(response.getPrescription().getFirst().getIdentifier()));
         var prescriptionFilter = new PrescriptionFilter(documentId,null,null);
         var result = EPrescriptionMapper.mapResponse("1111111118^^^&2.16.17.710.802.1000.990.1.500&ISO", prescriptionFilter, response,null);
