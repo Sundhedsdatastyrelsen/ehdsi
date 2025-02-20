cfb443c3d9b1465b63f1322bdd25b43d9986a6b6
Christian Frausing Brams <cfb@globeteam.com>
2025-02-20 14:55:15 +0100

    Added error handling in controllers to avoid leaking errors to NCP

--- country-a-service/epps-application/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/controller/PrescriptionController.java
+++ country-a-service/epps-application/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/controller/PrescriptionController.java
@@ -12,6 +12,7 @@ import dk.sundhedsdatastyrelsen.ncpeh.service.PrescriptionService;
 import dk.sundhedsdatastyrelsen.ncpeh.service.PrescriptionService.PrescriptionFilter;
 import dk.sundhedsdatastyrelsen.ncpeh.service.exception.DataRequirementException;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @RestController
 public class PrescriptionController {
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(PrescriptionController.class);
     private final PrescriptionService prescriptionService;
 
     public PrescriptionController(PrescriptionService prescriptionService) {
@@ -50,7 +52,17 @@ public class PrescriptionController {
         try {
             prescriptionService.submitDispensation(request.getPatientId(), Utils.readXmlDocument(request.getDocument()), TestIdentities.apotekerChrisChristoffersen);
         } catch (SAXException e) {
-            throw new DataRequirementException("Could not read XML document in request");
+            logger.error("Could not read XML document in request", e);
+        } catch (Exception e) {
+            // The received dispensation is just a receipt, error handling needs to make sure the information is
+            // communicated to relevant error handling parties - and not throw an error, since the NCP service does not
+            // regard failure as anything other than the service being down.
+            // TODO Logs here might contain patient information, should be stored somewhere with better security
+            // TODO Logs are probably not the best tool for communicating this
+            logger.error(String.format("Failed in handling dispensation request for patient id %s, with classcode %s", request.getPatientId(), request.getClassCode()
+                .toString()));
+            logger.error(String.format("SOAP Header: %s", request.getSoapHeader()));
+            logger.error(String.format("Request document: %s", request.getDocument()));
         }
     }
 
@@ -64,7 +76,18 @@ public class PrescriptionController {
                     .getPatientId(), Utils.readXmlDocument(request.getDispensationToDiscard()
                     .getDocument()), TestIdentities.apotekerChrisChristoffersen);
         } catch (SAXException e) {
-            throw new DataRequirementException("Could not read XML document in request");
+            logger.error("Could not read XML document in request");
+        } catch (Exception e) {
+            // The received discard request is just a receipt, error handling needs to make sure the information is
+            // communicated to relevant error handling parties - and not throw an error, since the NCP service does not
+            // regard failure as anything other than the service being down.
+            // TODO Logs here might contain patient information, should be stored somewhere with better security
+            // TODO Logs are probably not the best tool for communicating this
+            logger.error(String.format("Failed in handling discard request for patient id %s, with classcode %s", request.getDispensationToDiscard()
+                .getPatientId(), request.getDispensationToDiscard().getClassCode()
+                .toString()));
+            logger.error(String.format("SOAP Header: %s", request.getSoapHeader()));
+            logger.error(String.format("Request document: %s", request.getDispensationToDiscard().getDocument()));
         }
     }
 }
