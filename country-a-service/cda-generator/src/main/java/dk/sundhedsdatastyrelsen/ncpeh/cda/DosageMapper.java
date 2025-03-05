a9475b63a5706503d9cebac9c94ce177834113d7
Jakob Vase <jakobvase@gmail.com>
2025-03-05 11:30:06 +0100

    109: add description of what we're mapping from and to to dosagemapper

--- country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/DosageMapper.java
+++ country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/DosageMapper.java
@@ -11,6 +11,78 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Optional;
 
+/// Maps from the FMK ordination representation of dosage to the eHDSI substanceAdministration
+/// representation of dosage.
+///
+/// We're trying to map from something that looks like this:
+///
+/// ```xml
+/// <Dosage>
+///     <!-- Bemærk fra og med Extension E2 vil UnitText(s) elementer ikke have en source attribut -->
+///     <UnitTexts source="Local">
+///         <Singular>tablet</Singular>
+///         <Plural>tabletter</Plural>
+///     </UnitTexts>
+///     <StructuresFixed>
+///         <Structure>
+///             <IterationInterval>1</IterationInterval>
+///             <StartDate>2014-11-18</StartDate>
+///             <EndDate>2014-11-30</EndDate>
+///             <Day>
+///                 <Number>1</Number>
+///                 <Dose>
+///                     <Time>morning</Time>
+///                     <Quantity>2</Quantity>
+///                 </Dose>
+///                 <Dose>
+///                     <Time>evening</Time>
+///                     <Quantity>2</Quantity>
+///                 </Dose>
+///             </Day>
+///             <DosageTranslation>
+///                 <ShortText>1 tablet morgen og aften</ShortText>
+///                 <LongText>Doseringsforløbet starter mandag den 18. november 2013 og gentages hver dag:
+///                     Doseringsforløb:
+///                     1 tablet morgen og aften</LongText>
+///                 <AverageDailyDosage>2</AverageDailyDosage>
+///             </DosageTranslation>
+///         </Structure>
+///     </StructuresFixed>
+///     <Type>fast</Type>
+/// </Dosage>
+///```
+///
+/// To something that looks like this:
+///
+/// ```xml
+/// <substanceAdministration classCode="SBADM" moodCode="INT">
+///     ...
+///     <effectiveTime xsi:type="IVL_TS">
+///         <low value="20141118" />
+///         <high value="20141130" />
+///     </effectiveTime>
+///     <effectiveTime operator="A" type="PIVL_TS">
+///         <period unit="h" value="12" />
+///     </effectiveTime>
+///     <doseQuantity value="2"/>
+///     ...
+/// </substanceAdministration>
+///```
+///
+/// There are things that can be modeled in the Danish model, that cannot yet be
+/// modeled in the ehdsi model.
+///
+/// - doses of different sizes (tapered dosing, some cases of handling 2.25 mg when only 2 and 2.5 mg tablets exist).
+///   At some point, `subordinate substanceAdministration` may be added that could handle this, but they are not
+///   implemented yet.
+/// - doses that repeat at specific times of the day. In the danish model, it could say "take a pill in the morning and
+///   one around lunch for 7 days". There is the possibility of using the `SEXP_TS` type to design a set that includes
+///   only these two times, but there are no examples of this.
+/// - dosage that includes breaks. Birth control pills are an example of this, but it could be as arbitrary as "take 1
+///   pill on day 1, one pill on day 3, and one pill on day 7. Repeat every 2 weeks". This could also potentially be
+///   solved with the `SEXP_TS` type, and there _is_ an example of birth control pills [here](https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?spaceKey=EHDSI&title=General),
+///   but that's the only one I've been able to find, it only covers that specific case, and it's ambiguous. It doesn't
+///   specify when to start and end within the 28 days.
 @Slf4j
 public class DosageMapper {
     public static Dosage model(DosageForResponseType dosage) {
