c31a5a70626880537e8e69aac1bfff8ee23cf3c5
Jakob Vase <jakobvase@gmail.com>
2025-04-02 13:45:51 +0200

    change to display prescription id as id for L3 and L1
    We got an error during the testing, that the ID we sent for the metadata
    was not in the document we then returned afterwards. This fixes that.
    Remark that we are still using a catch-all oid for these ids, that
    should be changed later.

--- country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL1Mapper.java
+++ country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL1Mapper.java
@@ -1,21 +1,30 @@
 package dk.sundhedsdatastyrelsen.ncpeh.cda;
 
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.CdaId;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.model.EPrescriptionL1;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.model.EPrescriptionL3;
 
 import java.util.Base64;
 
-public class EPrescriptionL1Mapper {
+public final class EPrescriptionL1Mapper {
+    private EPrescriptionL1Mapper() {
+    }
+
     public static EPrescriptionL1 model(EPrescriptionL3Input input) throws MapperException {
         return model(EPrescriptionL3Mapper.model(input));
     }
 
     public static EPrescriptionL1 model(EPrescriptionL3 l3Model) throws MapperException {
-        var pdfModel = EPrescriptionPdfMapper.map(l3Model);
+        var modelWithL1Id = l3Model.toBuilder()
+            .documentId(new CdaId(
+                Oid.DK_EPRESCRIPTION_REPOSITORY_ID, EPrescriptionDocumentIdMapper.level1DocumentId(l3Model.getPrescriptionId()
+                .getExtension())))
+            .build();
+        var pdfModel = EPrescriptionPdfMapper.map(modelWithL1Id);
         var pdf = EPrescriptionPdfGenerator.generate(pdfModel);
         var base64Pdf = Base64.getEncoder().encodeToString(pdf);
         return EPrescriptionL1.builder()
-            .l3(l3Model)
+            .l3(modelWithL1Id)
             .base64EncodedDocument(base64Pdf)
             .build();
     }
