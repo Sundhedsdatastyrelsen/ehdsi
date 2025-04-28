bd934716076923c6dfda38883bec8f122e17eab1
Jakob Vase <jakobvase@gmail.com>
2025-04-28 13:03:27 +0200

    141: enable logging of unstructured dosage reasons

--- country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL3Generator.java
+++ country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL3Generator.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 public class EPrescriptionL3Generator {
+    private EPrescriptionL3Generator() {
+    }
+
     public static String generate(EPrescriptionL3 dataModel) throws IOException, TemplateException {
         var template = FreemarkerConfiguration.config().getTemplate("eprescription-cda-l3.ftlx");
         var writer = new StringWriter();
