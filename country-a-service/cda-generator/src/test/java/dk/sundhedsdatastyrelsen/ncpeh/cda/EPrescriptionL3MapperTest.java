a1d60b8c6ba871c6890509344bbab502f5521da9
Jakob Vase <jakobvase@gmail.com>
2025-05-30 18:17:50 +0200

    245: inner packages

--- country-a-service/cda-generator/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL3MapperTest.java
+++ country-a-service/cda-generator/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/cda/EPrescriptionL3MapperTest.java
@@ -5,6 +5,9 @@ import dk.sundhedsdatastyrelsen.ncpeh.testing.shared.FmkResponseStorage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 class EPrescriptionL3MapperTest {
     static EPrescriptionL3 getModel() {
         try {
@@ -12,7 +15,7 @@ class EPrescriptionL3MapperTest {
             var medicationResponse = FmkResponseStorage.storedDrugMedications(FmkResponseStorage.rawResponseCprs()
                 .get(2));
 
-            return EPrescriptionL3Mapper.model(new EPrescriptionL3Input(response, 0, medicationResponse, "FIN", "Manufacturer"));
+            return EPrescriptionL3Mapper.model(new EPrescriptionL3Input(response, 0, medicationResponse, "FIN", "1", "Manufacturer"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -36,6 +39,33 @@ class EPrescriptionL3MapperTest {
         Assertions.assertNotNull(model.getMedicationStartTime());
     }
 
+    @Test
+    void nullSubpackagesIsHandled() throws Exception {
+        var cpr = FmkResponseStorage.rawResponseCprs().get(2);
+        var model = EPrescriptionL3Mapper.model(
+            new EPrescriptionL3Input(
+                FmkResponseStorage.storedPrescriptions(cpr),
+                0,
+                FmkResponseStorage.storedDrugMedications(cpr),
+                "FIN",
+                null,
+                "Manufacturer"));
+        assertThat(model.getProduct().getPackageInfo().getWrappedIn(), is(nullValue()));
+        assertThat(model.getProduct().getPackageInfo().getValue(), is("100"));
+    }
 
-    /// etc....
+    @Test
+    void emptySubpackagesIsHandled() throws Exception {
+        var cpr = FmkResponseStorage.rawResponseCprs().get(2);
+        var model = EPrescriptionL3Mapper.model(
+            new EPrescriptionL3Input(
+                FmkResponseStorage.storedPrescriptions(cpr),
+                0,
+                FmkResponseStorage.storedDrugMedications(cpr),
+                "FIN",
+                "",
+                "Manufacturer"));
+        assertThat(model.getProduct().getPackageInfo().getWrappedIn(), is(nullValue()));
+        assertThat(model.getProduct().getPackageInfo().getValue(), is("100"));
+    }
 }
