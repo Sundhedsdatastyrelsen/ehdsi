a5302ddb4c80f7e5d22d761e25773dabaf225a60
Christian Frausing Brams <christian@brams.dk>
2025-06-17 15:17:54 +0200

    Follow java naming conventions

--- country-a-service/epps-api/epps-sdp-api/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/sdp/model/DiagnosisEntry.java
+++ country-a-service/epps-api/epps-sdp-api/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/sdp/model/DiagnosisEntry.java
@@ -1,15 +1,15 @@
 package dk.sundhedsdatastyrelsen.ncpeh.sdp.model;
 
 public record DiagnosisEntry(
-    String EkEncounter, //Reference to an Encounter with the patient (Original title: DW_EK_Kontakt)
-    String SkEncounter, //Reference to an Encounter with the patient (Original title: DW_SK_Kontakt)
-    String SkDiagnosis, //Reference to a diagnosis (not primary key, multiple rows have the same Diagnosis), (Original title: DW_SK_Diagnose)
-    String SkDiagnosisParent, //Reference to a diagnosis that is a parent
-    String Code, //Diagnosis SKS-Code (https://medinfo.dk/sks/brows.php)
-    String Text, //Diagnosis text
-    String TypeCode, //Short code for data, like "A" has a value in TypeText of Aktionsdiagnose
-    String TypeText,
-    String LaterDisproven //Values: Nej, Ja, Empty/Null
+    String ekEncounter, //Reference to an Encounter with the patient (Original title: DW_EK_Kontakt)
+    String skEncounter, //Reference to an Encounter with the patient (Original title: DW_SK_Kontakt)
+    String skDiagnosis, //Reference to a diagnosis (not primary key, multiple rows have the same Diagnosis), (Original title: DW_SK_Diagnose)
+    String skDiagnosisParent, //Reference to a diagnosis that is a parent
+    String code, //Diagnosis SKS-Code (https://medinfo.dk/sks/brows.php)
+    String text, //Diagnosis text
+    String typeCode, //Short code for data, like "A" has a value in TypeText of Aktionsdiagnose
+    String typeText,
+    String laterDisproven //Values: Nej, Ja, Empty/Null
 ) {
 
 }
--- country-a-service/epps-api/epps-sdp-api/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/sdp/model/ImplantRegisterEntry.java
+++ country-a-service/epps-api/epps-sdp-api/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/sdp/model/ImplantRegisterEntry.java
@@ -4,25 +4,25 @@ import java.time.OffsetDateTime;
 
 public record ImplantRegisterEntry(
     String cpr, // Patient ID
-    String RegionCode, //Regionskode
-    String ReportingSor, //SOR code for reporting entity (Sundhedsvæsenets Organisationsregistor)
-    String ReportingShak, //SHAK code for reporting entity (likely to be null if not from hospital) (Sygehus-afdelingsklassifikationer)
-    String OwnerType, //Possible values: region, privat, Ukendt
-    OffsetDateTime OperationDate,
-    String OperationSksCode, //Operation SKS code, lookup in https://medinfo.dk/sks/brows.php, This field contains a free text mix of codes and text, like: "KHAD60 - Udskiftning af brystprotese + KHAD50 - Fjernelse af brystprotese + KWAB01 - Implantat fjernet + KWAA01 - Implantat indsat" or "KWAA01 - Implantat indsat + KNFB20 - Primær indsættelse af ucementeret totalprotese i hofteled"
-    String Status, //Values: Empty/Null, "Ændret Registrering", "Slettet registrering", "Implantat indsat", "Ingen registrering", "Implantat udtaget"
-    Integer NumberOfImplants,
-    Boolean RightSideImplantet, //This and the 2 following are exclusive. Some exists where all are false, but none where more than 1 is true
-    Boolean LeftSideImplantet,
-    Boolean DoubleSidedImplant,
-    String ProductNumber, //Sometimes null/empty, or just "." or "-". Sometimes actual product numbers (that includes symbols)
-    String BatchNumber, //Same rules as above
-    String SerialNumber,
-    String RegistryStandard, //Contains free text description of how it was registered. Sometimes references to standards like GS1, or DS1, but numerous spelling errors denote that this is not a coded field.
-    OffsetDateTime RegistryTime,
-    OffsetDateTime ProductionTime,
-    OffsetDateTime ExpiryTime, //Expiry for the implant, not the data
-    Boolean Error
+    String regionCode, //Regionskode
+    String reportingSor, //SOR code for reporting entity (Sundhedsvæsenets Organisationsregistor)
+    String reportingShak, //SHAK code for reporting entity (likely to be null if not from hospital) (Sygehus-afdelingsklassifikationer)
+    String ownerType, //Possible values: region, privat, Ukendt
+    OffsetDateTime operationDate,
+    String operationSksCode, //Operation SKS code, lookup in https://medinfo.dk/sks/brows.php, This field contains a free text mix of codes and text, like: "KHAD60 - Udskiftning af brystprotese + KHAD50 - Fjernelse af brystprotese + KWAB01 - Implantat fjernet + KWAA01 - Implantat indsat" or "KWAA01 - Implantat indsat + KNFB20 - Primær indsættelse af ucementeret totalprotese i hofteled"
+    String status, //Values: Empty/Null, "Ændret Registrering", "Slettet registrering", "Implantat indsat", "Ingen registrering", "Implantat udtaget"
+    Integer numberOfImplants,
+    Boolean rightSideImplantet, //This and the 2 following are exclusive. Some exists where all are false, but none where more than 1 is true
+    Boolean leftSideImplantet,
+    Boolean doubleSidedImplant,
+    String productNumber, //Sometimes null/empty, or just "." or "-". Sometimes actual product numbers (that includes symbols)
+    String batchNumber, //Same rules as above
+    String serialNumber,
+    String registryStandard, //Contains free text description of how it was registered. Sometimes references to standards like GS1, or DS1, but numerous spelling errors denote that this is not a coded field.
+    OffsetDateTime registryTime,
+    OffsetDateTime productionTime,
+    OffsetDateTime expiryTime, //Expiry for the implant, not the data
+    Boolean error
 ) {
 
 }
