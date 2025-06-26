1781bc1ea8851a076a0ab9f20d72209b0b6cd038
Hans Bugge Grathwohl <hbg@globeteam.com>
2025-06-26 15:09:10 +0200

    add observability for NCP

--- NCP/tsam-synchronizer/entrypoint.sh
+++ NCP/tsam-synchronizer/entrypoint.sh
@@ -9,5 +9,14 @@ export DB_PASSWORD; DB_PASSWORD=$(</run/secrets/db_password)
 export TSAM_SYNC_USERNAME; TSAM_SYNC_USERNAME=$(</run/secrets/cts_username)
 export TSAM_SYNC_PASSWORD; TSAM_SYNC_PASSWORD=$(</run/secrets/cts_password)
 
-# Execute the original command
-exec "$@"
+# Set OpenTelemetry environment variables
+export OTEL_SERVICE_NAME="tsam-synchronizer"
+export OTEL_EXPORTER_OTLP_ENDPOINT="http://jaeger:4317"
+export OTEL_EXPORTER_OTLP_PROTOCOL="grpc"
+export OTEL_TRACES_SAMPLER="always_on"
+export OTEL_METRICS_EXPORTER="none"
+export OTEL_LOGS_EXPORTER="none"
+export OTEL_PROPAGATORS="tracecontext,baggage"
+
+# Execute the original command with OpenTelemetry agent
+exec java -javaagent:/opt/opentelemetry-javaagent.jar "$@"
