Sundhedsdatastyrelsen/ehdsi

commit cf27237e4063ebf2f6f8ac3fb9b37e90d533a8a7

Patient Summary wiring everything together (#290)
Builds on top of merge request #274, which was closed in favor
of this. The patient summary should now be accessible for
users, although it might error when actually generating it, as it's
probably not valid yet.

country-a-service/epps-service/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/service/PrescriptionService.java
@@ -24,7 +24,9 @@ import dk.sundhedsdatastyrelsen.ncpeh.cda.EPrescriptionL3Generator;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.EPrescriptionL3Input;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.EPrescriptionL3Mapper;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.MapperException;
+import dk.sundhedsdatastyrelsen.ncpeh.cda.Oid;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.Utils;
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.CdaId;
 import dk.sundhedsdatastyrelsen.ncpeh.client.AuthorizationRegistryClient;
 import dk.sundhedsdatastyrelsen.ncpeh.client.FmkClient;
 import dk.sundhedsdatastyrelsen.ncpeh.locallms.DataProvider;
@@ -110,8 +112,11 @@ public class PrescriptionService {
         }
 
         public static PrescriptionFilter fromRootedId(String rootedDocumentId, OffsetDateTime createdBefore, OffsetDateTime createdAfter) {
-            var documentId = EPrescriptionMetadataMapper.fromRootedId(rootedDocumentId);
-            return new PrescriptionFilter(documentId, createdBefore, createdAfter);
+            var documentId = CdaId.fromDocumentId(rootedDocumentId);
+            if (documentId.getRootOid() != Oid.DK_EPRESCRIPTION_REPOSITORY_ID) {
+                throw new CountryAException(HttpStatus.BAD_REQUEST, "Document repository in document ID is not ePrescription.");
+            }
+            return new PrescriptionFilter(documentId.getExtension(), createdBefore, createdAfter);
         }
     }
 

NCP/openncp-national-connector/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/nationalconnector/xca/DocumentSearch.java
@@ -1,8 +1,8 @@
 package dk.sundhedsdatastyrelsen.ncpeh.nationalconnector.xca;
 
 import dk.sundhedsdatastyrelsen.ncpeh.ApiException;
-import dk.sundhedsdatastyrelsen.ncpeh.api.model.PostFetchDocumentRequest;
 import dk.sundhedsdatastyrelsen.ncpeh.api.model.PostFindEPrescriptionDocumentsRequest;
+import dk.sundhedsdatastyrelsen.ncpeh.api.model.PostFetchDocumentRequest;
 import dk.sundhedsdatastyrelsen.ncpeh.nationalconnector.CountryAService;
 import dk.sundhedsdatastyrelsen.ncpeh.nationalconnector.Utils;
 import eu.europa.ec.sante.openncp.common.ClassCode;
@@ -10,7 +10,6 @@ import eu.europa.ec.sante.openncp.common.error.OpenNCPErrorCode;
 import eu.europa.ec.sante.openncp.core.common.ihe.NationalConnectorInterface;
 import eu.europa.ec.sante.openncp.core.common.ihe.assertionvalidator.exceptions.InsufficientRightsException;
 import eu.europa.ec.sante.openncp.core.common.ihe.datamodel.SimpleConfidentialityEnum;
-import eu.europa.ec.sante.openncp.core.common.ihe.datamodel.SubstitutionCodeEnum;
 import eu.europa.ec.sante.openncp.core.common.ihe.datamodel.xds.*;
 import eu.europa.ec.sante.openncp.core.common.ihe.exception.NIException;
 import eu.europa.ec.sante.openncp.core.common.ihe.transformation.util.XmlUtil;
@@ -76,7 +75,56 @@ public class DocumentSearch implements NationalConnectorInterface, DocumentSearc
 
     @Override
     public DocumentAssociation<PSDocumentMetaData> getPSDocumentList(SearchCriteria searchCriteria) throws NIException, InsufficientRightsException {
-        throw new UnsupportedOperationException();
+        try {
+            final var soapHeader = this.soapHeader;
+            logger.info("Querying Country A service for patient summary documents...");
+            // Patient summary has the same arguments as EP, so we use the same class.
+            final var request = new PostFindEPrescriptionDocumentsRequest()
+                .patientId(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.PATIENT_ID))
+                .repositoryId(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.REPOSITORY_ID))
+                .documentId(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.DOCUMENT_ID))
+                .maximumSize(Utils.parseLong(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.MAXIMUM_SIZE)))
+                .createdBefore(Utils.parseOffsetDateTime(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.CREATED_BEFORE)))
+                .createdAfter(Utils.parseOffsetDateTime(searchCriteria.getCriteriaValue(SearchCriteria.Criteria.CREATED_AFTER)))
+                .soapHeader(Utils.elementToString(soapHeader));
+            final var md = CountryAService.api().postFindPatientSummaryDocument(request);
+            logger.info("Got well-formed response from Country A service.");
+            return DocumentFactory.createDocumentAssociation(
+                    DocumentFactory.createPSDocumentXML(
+                        md.getLevel3().getId(),
+                        md.getLevel3().getPatientId(),
+                        Utils.offsetDateTimeToDate(md.getLevel3().getEffectiveTime()),
+                        md.getLevel3().getRepositoryId(),
+                        md.getLevel3().getTitle(),
+                        md.getLevel3().getAuthor(),
+                        SimpleConfidentialityEnum.findByCode(md.getLevel3().getConfidentiality().getConfidentialityCode()),
+                        md.getLevel3().getLanguage(),
+                        md.getLevel3().getSize(),
+                        md.getLevel3().getHash()
+                    ),
+                    DocumentFactory.createPSDocumentPDF(
+                        md.getLevel1().getId(),
+                        md.getLevel1().getPatientId(),
+                        Utils.offsetDateTimeToDate(md.getLevel1().getEffectiveTime()),
+                        md.getLevel1().getRepositoryId(),
+                        md.getLevel1().getTitle(),
+                        md.getLevel1().getAuthor(),
+                        SimpleConfidentialityEnum.findByCode(md.getLevel1().getConfidentiality().getConfidentialityCode()),
+                        md.getLevel1().getLanguage(),
+                        md.getLevel1().getSize(),
+                        md.getLevel1().getHash()
+                    ));
+        } catch (ApiException e) {
+            if (e.getCode() == 0) {
+                throw new NIException(OpenNCPErrorCode.ERROR_GENERIC, "Could not establish connection with service");
+            } else {
+                throw new NIException(OpenNCPErrorCode.ERROR_GENERIC, String.format(
+                    "Error, status: %s, body: %s",
+                    e.getCode(),
+                    e.getResponseBody()
+                ));
+            }
+        }
     }
 
     static List<DocumentAssociation<EPDocumentMetaData>> getEPDocumentListFromCountryA(SearchCriteria searchCriteria, Element soapHeader) throws NIException {

NCP/openncp-national-connector/src/main/resources/country-a-api.yml
@@ -1,14 +1,12 @@
 openapi: 3.0.3
 info:
-  version: '1.0'
+  version: "1.0"
   title: NCP API
 
 servers:
   - url: http://localhost:8080
 
-
 paths:
-
   # 55
   /api/find-patients/:
     post:
@@ -20,21 +18,21 @@ paths:
           application/json:
             schema:
               allOf:
-                - $ref: '#/components/schemas/FindPatientsRequest'
-                - $ref: '#/components/schemas/SoapHeader'
+                - $ref: "#/components/schemas/FindPatientsRequest"
+                - $ref: "#/components/schemas/SoapHeader"
       responses:
         200:
           description: Patient info
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/FindPatientsResponse'
+                $ref: "#/components/schemas/FindPatientsResponse"
         400:
           description: 400 BadRequest
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
 
   # 39
   /api/fetch-document/:
@@ -49,8 +47,8 @@ paths:
           application/json:
             schema:
               allOf:
-                - $ref: '#/components/schemas/FetchDocumentRequest'
-                - $ref: '#/components/schemas/SoapHeader'
+                - $ref: "#/components/schemas/FetchDocumentRequest"
+                - $ref: "#/components/schemas/SoapHeader"
       responses:
         200:
           description: Documents
@@ -59,21 +57,20 @@ paths:
               schema:
                 type: array
                 items:
-                  $ref: '#/components/schemas/EpsosDocument'
+                  $ref: "#/components/schemas/EpsosDocument"
         400:
           description: 400 BadRequest
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
         404:
           description: Could not locate document
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
 
-  # 38
   /api/find-eprescription-documents/:
     post:
       summary: |
@@ -86,9 +83,8 @@ paths:
           application/json:
             schema:
               allOf:
-                - $ref: '#/components/schemas/FindEprescriptionDocumentsRequest'
-                - $ref: '#/components/schemas/SoapHeader'
-              x-all-of-name: "FindEprescriptionDocumentsRequest"
+                - $ref: "#/components/schemas/FindDocumentsRequest"
+                - $ref: "#/components/schemas/SoapHeader"
       responses:
         200:
           description: ePrescription document metadata matching criteria.
@@ -97,13 +93,41 @@ paths:
               schema:
                 type: array
                 items:
-                  $ref: '#/components/schemas/DocumentAssociationForEPrescriptionDocumentMetadata'
+                  $ref: "#/components/schemas/DocumentAssociationForEPrescriptionDocumentMetadata"
         400:
           description: 400 BadRequest
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
+
+  /api/find-patient-summary-document/:
+    post:
+      summary: |
+        Search for a patient summary matching the given search criteria.
+        Corresponds to XCA ITI-38 endpoint.
+      operationId: postFindPatientSummaryDocument
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              allOf:
+                - $ref: "#/components/schemas/FindDocumentsRequest"
+                - $ref: "#/components/schemas/SoapHeader"
+      responses:
+        200:
+          description: Patient summary document metadata matching criteria.
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/DocumentAssociationForPatientSummaryDocumentMetadata"
+        400:
+          description: 400 BadRequest
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/Error"
 
   /api/edispensation/submit:
     post:
@@ -118,8 +142,8 @@ paths:
           application/json:
             schema:
               allOf:
-                - $ref: '#/components/schemas/EpsosDocument'
-                - $ref: '#/components/schemas/SoapHeader'
+                - $ref: "#/components/schemas/EpsosDocument"
+                - $ref: "#/components/schemas/SoapHeader"
       responses:
         204:
           description: Dispensation submitted successfully.
@@ -128,7 +152,7 @@ paths:
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
 
   /api/edispensation/discard:
     post:
@@ -141,13 +165,13 @@ paths:
           application/json:
             schema:
               allOf:
-                - $ref: '#/components/schemas/SoapHeader'
+                - $ref: "#/components/schemas/SoapHeader"
                 - type: object
                   properties:
                     disardDispenseDetails:
-                      $ref: '#/components/schemas/DiscardDispenseDetails'
+                      $ref: "#/components/schemas/DiscardDispenseDetails"
                     dispensationToDiscard:
-                      $ref: '#/components/schemas/EpsosDocument'
+                      $ref: "#/components/schemas/EpsosDocument"
 
       responses:
         204:
@@ -157,7 +181,7 @@ paths:
           content:
             application/json:
               schema:
-                $ref: '#/components/schemas/Error'
+                $ref: "#/components/schemas/Error"
 
 components:
   schemas:
@@ -188,7 +212,7 @@ components:
         patients:
           type: array
           items:
-            $ref: '#/components/schemas/PatientDemographics'
+            $ref: "#/components/schemas/PatientDemographics"
         patientsNotFound:
           type: array
           items:
@@ -215,7 +239,7 @@ components:
           type: string
           format: date-time
 
-    FindEprescriptionDocumentsRequest:
+    FindDocumentsRequest:
       type: object
       required:
         - patientId
@@ -250,7 +274,7 @@ components:
           type: string
           format: date
         administrativeGender:
-          $ref: '#/components/schemas/Gender'
+          $ref: "#/components/schemas/Gender"
         city:
           type: string
         country:
@@ -304,7 +328,7 @@ components:
           type: string
           format: xml
         classCode:
-          $ref: '#/components/schemas/ClassCode'
+          $ref: "#/components/schemas/ClassCode"
 
     EpsosDocumentMetadata:
       type: object
@@ -317,12 +341,12 @@ components:
         patientId:
           type: string
         format:
-          $ref: '#/components/schemas/DocumentFormat'
+          $ref: "#/components/schemas/DocumentFormat"
         effectiveTime:
           type: string
           format: date-time
         classCode:
-          $ref: '#/components/schemas/ClassCode'
+          $ref: "#/components/schemas/ClassCode"
         repositoryId:
           type: string
         title:
@@ -337,7 +361,7 @@ components:
         hash:
           type: string
         confidentiality:
-          $ref: '#/components/schemas/ConfidentialityMetadata'
+          $ref: "#/components/schemas/ConfidentialityMetadata"
 
     DocumentAssociationForEPrescriptionDocumentMetadata:
       type: object
@@ -347,14 +371,14 @@ components:
         - level3
       properties:
         level3:
-          $ref: '#/components/schemas/EPrescriptionDocumentMetadata'
+          $ref: "#/components/schemas/EPrescriptionDocumentMetadata"
         level1:
-          $ref: '#/components/schemas/EPrescriptionDocumentMetadata'
+          $ref: "#/components/schemas/EPrescriptionDocumentMetadata"
 
     EPrescriptionDocumentMetadata:
       description: Metadata for an ePrescription document.
       allOf:
-        - $ref: '#/components/schemas/EpsosDocumentMetadata'
+        - $ref: "#/components/schemas/EpsosDocumentMetadata"
         - type: object
           properties:
             description:
@@ -380,6 +404,18 @@ components:
             substitutionDisplayName:
               type: string
 
+    DocumentAssociationForPatientSummaryDocumentMetadata:
+      type: object
+      description: Document association for metadata for a patient summary document (both L1 and L3).
+      required:
+        - level1
+        - level3
+      properties:
+        level3:
+          $ref: "#/components/schemas/EpsosDocumentMetadata"
+        level1:
+          $ref: "#/components/schemas/EpsosDocumentMetadata"
+
     ConfidentialityMetadata:
       type: object
       properties:
@@ -392,8 +428,10 @@ components:
       description: LOINC code for the document type.
       type: string
       enum:
+        # These were taken from https://webgate.ec.europa.eu/fpfis/wikis/spaces/EHDSI/pages/912791653/Enable+the+exchange+of+original+clinical+documents
         # For PoC we only support ePrescriptions and eDispensations:
         - "57833-6" # Prescription for medication https://loinc.org/57833-6
+        - "60591-5" # Patient Summary https://loinc.org/60591-5
         - "60593-1" # Medication dispensed.extended Document https://loinc.org/60593-1
 
     DocumentFormat:

country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/Oid.java
@@ -8,6 +8,11 @@ public enum Oid {
      */
     DK_EPRESCRIPTION_REPOSITORY_ID("1.2.208.176.7.2.3"),
     /**
+     * Patient summary document repository id.
+     * TODO get a real oid here.
+     */
+    DK_PATIENT_SUMMARY_REPOSITORY_ID("1.2.208.176.7.2.987654321"),
+    /**
      * Registries used in the Danish health by data exchange.
      * <a href="https://oid-base.com/get/1.2.208.176.1">Registered.</a>
      */

country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/PatientSummaryInput.java
@@ -1,0 +1,9 @@
+package dk.sundhedsdatastyrelsen.ncpeh.cda;
+
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.PreferredHealthProfessional;
+
+public record PatientSummaryInput(
+    String documentId,
+    PreferredHealthProfessional preferredHealthProfessional
+) {
+}

country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/PatientSummaryL3Generator.java
@@ -1,10 +1,17 @@
 package dk.sundhedsdatastyrelsen.ncpeh.cda;
 
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.CdaCode;
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.CdaId;
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.Name;
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.Patient;
 import dk.sundhedsdatastyrelsen.ncpeh.cda.model.PatientSummaryL3;
 import freemarker.template.TemplateException;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 
 public class PatientSummaryL3Generator {
     private PatientSummaryL3Generator() {
@@ -15,5 +22,34 @@ public class PatientSummaryL3Generator {
         var writer = new StringWriter();
         template.process(dataModel, writer);
         return writer.toString();
+    }
+
+    @WithSpan
+    public static String generate(PatientSummaryInput input) throws MapperException, TemplateException, IOException {
+        var model = inputToModel(input);
+        return generate(model);
+    }
+
+    private static PatientSummaryL3 inputToModel(PatientSummaryInput input) {
+        return PatientSummaryL3.builder()
+            .documentId(CdaId.fromDocumentId(input.documentId()))
+            .effectiveTime(OffsetDateTime.now())
+            // TODO is the title correct?
+            .title("Patient Summary")
+            // TODO get the patient data somewhere instead of fake data.
+            .patient(Patient.builder()
+                .birthTime(LocalDate.of(1997, 3, 3))
+                .genderCode(CdaCode.builder()
+                    .codeSystem(Oid.ADMINISTRATIVE_GENDER)
+                    .codeSystemVersion("913-20091020")
+                    .code("F")
+                    .displayName("Female")
+                    .build())
+                .id(new CdaId(Oid.DK_CPR, "0303970000"))
+                .name(Name.fromFullName("Ghita Nørby"))
+                // TODO address
+                .build())
+            .preferredHp(input.preferredHealthProfessional())
+            .build();
     }
 }

country-a-service/cda-generator/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/cda/model/CdaId.java
@@ -4,7 +4,9 @@ import dk.sundhedsdatastyrelsen.ncpeh.cda.Oid;
 import lombok.NonNull;
 import lombok.Value;
 
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.UUID;
 
 @Value
@@ -32,5 +34,41 @@ public class CdaId {
             return rootUuid.toString().toUpperCase(Locale.ROOT);
         }
         throw new RuntimeException("invalid CdaId, should not happen");
+    }
+
+    /// Try to get an id from a string also containing the correct root "1.2.208.176.7.2.3^123456L3" -> "123456L3". If
+    /// it cannot be parsed, null is returned. See also the comment on [CdaId#toDocumentId(CdaId)].
+    public static CdaId fromDocumentId(String input) {
+        if (input == null) {
+            return null;
+        }
+
+        try {
+            return new CdaId(UUID.fromString(input));
+        } catch (IllegalArgumentException e) {
+            // Not a UUID then.
+        }
+
+        var parts = input.split("\\^", 2);
+        if (parts.length != 2) {
+            return null;
+        }
+
+        var oidString = parts[0];
+        var oid = Arrays.stream(Oid.values()).filter(v -> Objects.equals(v.value, oidString)).findFirst();
+        var id = parts[1];
+        if (id.trim().isEmpty() || oid.isEmpty()) {
+            return null;
+        }
+
+        return new CdaId(oid.get(), id);
+    }
+
+    /// We found during the Spring test of 2025 that the countries that were already in production expected us to pass
+    /// rooted ePrescription ids - so we do that now. The format is not documented anywhere I can find.
+    public static String toDocumentId(CdaId id) {
+        return id.getRootUuid() == null
+            ? String.format("%s^%s", id.getRootOid().value, id.getExtension())
+            : id.getRootUuid().toString();
     }
 }

country-a-service/cda-generator/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/cda/CdaIdTest.java
@@ -1,0 +1,17 @@
+package dk.sundhedsdatastyrelsen.ncpeh.cda;
+
+import dk.sundhedsdatastyrelsen.ncpeh.cda.model.CdaId;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class CdaIdTest {
+    @Test
+    void rootedIdTest() {
+        var id = new CdaId(Oid.DK_FMK_PRESCRIPTION, "1234L3");
+        assertThat(
+            CdaId.fromDocumentId(CdaId.toDocumentId(id)),
+            is(id));
+    }
+}

commit 11a747857e1984e29f6be36c343863d5bd21fa17

fix null-error in eprescription (#291)
Fixes a null error introduced in #290.

country-a-service/epps-service/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/service/PrescriptionService.java
@@ -113,10 +113,15 @@ public class PrescriptionService {
 
         public static PrescriptionFilter fromRootedId(String rootedDocumentId, OffsetDateTime createdBefore, OffsetDateTime createdAfter) {
             var documentId = CdaId.fromDocumentId(rootedDocumentId);
-            if (documentId.getRootOid() != Oid.DK_EPRESCRIPTION_REPOSITORY_ID) {
+            if (documentId != null && documentId.getRootOid() != Oid.DK_EPRESCRIPTION_REPOSITORY_ID) {
                 throw new CountryAException(HttpStatus.BAD_REQUEST, "Document repository in document ID is not ePrescription.");
             }
-            return new PrescriptionFilter(documentId.getExtension(), createdBefore, createdAfter);
+            return new PrescriptionFilter(
+                Optional.ofNullable(documentId)
+                    .map(CdaId::getExtension)
+                    .orElse(null),
+                createdBefore,
+                createdAfter);
         }
     }
 