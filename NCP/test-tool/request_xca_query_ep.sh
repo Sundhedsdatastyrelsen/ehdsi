6e20c564bc80838c475bb31f6261b97202096a50
Jakob Vase <jakobvase@gmail.com>
2025-07-30 12:54:30 +0200

    add query and retrieve for ps, rename ep versions

---
+++ NCP/test-tool/request_xca_query_ep.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+"$SCRIPT_DIR/run.sh" \
+    -X request-builder/build \
+    :hcp-template     "\"$SCRIPT_DIR/templates/xca/hcp.xml\"" \
+    :trc-template     "\"$SCRIPT_DIR/templates/xca/trc.xml\"" \
+    :request-template "\"$SCRIPT_DIR/templates/xca/query/request-ep.xml\"" \
+    :private-key      "\"$SCRIPT_DIR/testcert.p8.pem\"" \
+    :cert             "\"$SCRIPT_DIR/testcert.cer\""
