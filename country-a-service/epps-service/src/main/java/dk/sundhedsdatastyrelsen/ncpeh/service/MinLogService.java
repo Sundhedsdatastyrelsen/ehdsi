93dd7ff3409425d5dd91e10bac7ebe3b3ed46323
Hans Bugge Grathwohl <hbg@globeteam.com>
2025-08-03 22:01:16 +0200

    don't throw in the scheduled lambda

--- country-a-service/epps-service/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/service/MinLogService.java
+++ country-a-service/epps-service/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/service/MinLogService.java
@@ -13,7 +13,6 @@ import dk.sundhedsdatastyrelsen.ncpeh.client.NspClientException;
 import dk.sundhedsdatastyrelsen.ncpeh.jobqueue.JobQueue;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.metrics.ObservableLongUpDownCounter;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -30,6 +29,8 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @Service
 public class MinLogService implements AutoCloseable {
+    private static final String MINLOG_QUEUE_NAME = "minlog";
+
     private final MinLogClient minLogClient;
     private final JobQueue<LogEvent> jobQueue;
 
@@ -50,7 +51,7 @@ public class MinLogService implements AutoCloseable {
     ) throws SQLException {
         this.minLogClient = minLogClient;
         this.systemCaller = systemCaller;
-        this.jobQueue = JobQueue.open(jobQueueDatasource, "minlog", LogEvent.class, Duration.ofMinutes(2));
+        this.jobQueue = JobQueue.open(jobQueueDatasource, MINLOG_QUEUE_NAME, LogEvent.class, Duration.ofMinutes(3));
 
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
         // https://www.nspop.dk/spaces/web/pages/98456923/MinLog2+-+Min+Log+Registrering+-+Guide+til+anvendere#MinLog2MinLogRegistreringGuidetilanvendere-Operationer
@@ -58,14 +59,15 @@ public class MinLogService implements AutoCloseable {
         //
         // We will send up to 500 requests per 3 minutes.  This should keep us within the recommendations.
         scheduler.scheduleAtFixedRate(this::sendBatch, 0, 3, TimeUnit.MINUTES);
-        // We use otel metrics (prometheus) to keep track of the queue size, and whether it grows too much.
+        // We use otel metrics (prometheus) to keep track of the queue size.  There should be alerts if this number is too high.
         var meter = GlobalOpenTelemetry.meterBuilder("dk.sundhedsdatastyrelsen.ncpeh.service").build();
         this.queueSizeCounter = meter.upDownCounterBuilder("minlog.job_queue.size")
             .buildWithCallback(m -> m.record(jobQueue.size()));
     }
 
-    /// For test purposes.
+    /// Send the next batch of log entries to MinLog.
     ///
+    /// Public for testing purposes.
     /// @hidden
     public void sendBatch() {
         List<JobQueue.JobId> jobIds = null;
@@ -80,11 +82,11 @@ public class MinLogService implements AutoCloseable {
                 log.debug("Found no pending MinLog entries.");
             }
         } catch (Exception e) {
+            // we should not rethrow, otherwise our scheduler will stop on the first error.
             log.error("Something went wrong while sending log entries to MinLog.  Will retry.", e);
             if (jobIds != null) {
                 jobQueue.nack(jobIds);
             }
-            throw e;
         }
     }
 
@@ -133,9 +135,14 @@ public class MinLogService implements AutoCloseable {
         }
         var response = minLogClient.register(requestBuilder.build(), systemCaller);
         if (response.getNumberFailed() > 0) {
+            for (var failedEntry : response.getFailedLogDataEntries()) {
+                log.error("MinLog error: {}: {}", failedEntry.getFaultCode(), failedEntry.getFaultText());
+            }
             // TODO[hbg]: We need better error handling.  If some registrations fail, but not all, then we should re-queue the failing ones only.
             throw new NspClientException(
-                "MinLog registration failed. Failed: %d, Succeeded: %d".formatted(response.getNumberFailed(), response.getNumberAdded()));
+                "MinLog registration failed. Failed entries: %d, Succeeded entries: %d".formatted(
+                    response.getNumberFailed(),
+                    response.getNumberAdded()));
         }
     }
 
--- country-a-service/integration-tests/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/MinLogIT.java
+++ country-a-service/integration-tests/src/test/java/dk/sundhedsdatastyrelsen/ncpeh/MinLogIT.java
@@ -2,31 +2,41 @@ package dk.sundhedsdatastyrelsen.ncpeh;
 
 import dk.nsp.test.idp.OrganizationIdentities;
 import dk.sundhedsdatastyrelsen.ncpeh.client.TestIdentities;
+import dk.sundhedsdatastyrelsen.ncpeh.jobqueue.JobQueue;
 import dk.sundhedsdatastyrelsen.ncpeh.service.MinLogService;
 import dk.sundhedsdatastyrelsen.ncpeh.testing.shared.MinLog;
 import org.junit.jupiter.api.Test;
-import org.sqlite.SQLiteDataSource;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MinLogIT {
     public static MinLogService minLogService() throws SQLException {
-        return new MinLogService(MinLog.apiClient(), OrganizationIdentities.sundhedsdatastyrelsen(), ds());
+        return minLogService(ds());
     }
 
-    static SQLiteDataSource ds() {
-        var ds = new SQLiteDataSource();
-        ds.setUrl("jdbc:sqlite::memory:");
-        return ds;
+    public static MinLogService minLogService(DataSource ds) throws SQLException {
+        return new MinLogService(MinLog.apiClient(), OrganizationIdentities.sundhedsdatastyrelsen(), ds);
+    }
+
+    static SingleConnectionDataSource ds() {
+        return new SingleConnectionDataSource("jdbc:sqlite::memory:", true);
     }
 
     @Test
     void testEvent() throws Exception {
-        try (var service = minLogService()) {
+        try (var ds = ds();
+             var service = minLogService(ds);
+             var q = JobQueue.open(ds, "minlog", null, null)) {
             service.logEventOnPatient(MinLog.CPR_JENS_JENSEN_READ_ONLY, "integrationstest", TestIdentities.foreignPharmacistChrisChristoffersen);
+            assertThat(q.size(), is(1L));
             assertDoesNotThrow(service::sendBatch);
+            assertThat(q.size(), is(0L));
         }
     }
 }
