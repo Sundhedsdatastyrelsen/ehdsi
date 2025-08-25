72a7c0cae66a265657139d40aec61359be764069
Jakob Vase <jakobvase@gmail.com>
2025-08-25 13:42:16 +0200

    add required properties to app properties

--- country-a-service/epps-application/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/config/AuthenticationServiceConfig.java
+++ country-a-service/epps-application/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/config/AuthenticationServiceConfig.java
@@ -3,6 +3,7 @@ package dk.sundhedsdatastyrelsen.ncpeh.config;
 import dk.sundhedsdatastyrelsen.ncpeh.authentication.AuthenticationService;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.Name;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -22,10 +23,15 @@ public record AuthenticationServiceConfig(
 
     @ConstructorBinding
     public AuthenticationServiceConfig(
+        @Name("endpoint.url")
         String uri,
+        @Name("keystore.path")
         String keystorePath,
+        @Name("keystore.alias")
         String keystoreAlias,
+        @Name("keystore.password")
         String keystorePassword,
+        @Name("issuer")
         String issuer
     ) throws IOException {
         this(URI.create(uri), new BufferedInputStream(Files.newInputStream(Path.of(keystorePath))), keystoreAlias, keystorePassword, issuer);
