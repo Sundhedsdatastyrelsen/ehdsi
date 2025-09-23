baae1dff8dfa6e3ca8ada6af669330d342e2d13b
Jakob Vase <jakobvase@gmail.com>
2025-09-23 12:55:32 +0200

    fix fsk

--- country-a-service/testing-shared/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/testing/shared/Fsk.java
+++ country-a-service/testing-shared/src/main/java/dk/sundhedsdatastyrelsen/ncpeh/testing/shared/Fsk.java
@@ -6,7 +6,7 @@ import dk.sundhedsdatastyrelsen.ncpeh.client.FskClient;
  * API client for FSK test environment
  */
 public class Fsk {
-    private static final String fskEndpointUri = "http://test2.ekstern-test.nspop.dk:8080/decoupling";
+    private static final String fskEndpointUri = "http://test1.ekstern-test.nspop.dk:8080/decoupling";
 
     /**
      * Jens Jensen is a test person vi can see in the test person overview. Do not edit.
@@ -14,7 +14,6 @@ public class Fsk {
     public static final String cprJensJensenReadOnly = "0408801111";
 
     public static final String documentJensJensenFskResponse = "1.2.208.176.43210.8.10.12^aa575bf2-fde6-434c-bd0c-ccf5a512680d";
-
 
     private static FskClient fskClient;
 
