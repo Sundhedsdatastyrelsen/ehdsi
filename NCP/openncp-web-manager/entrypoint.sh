85242b427207c3b9d719ccf4d70da0b703856ba5
Jakob Vase <jakobvase@gmail.com>
2026-01-29 13:54:43 +0100

    remove socat from docker images
    It is not used by us. It was something implemented by the openncp team
    to solve an issue we don't have.

--- NCP/openncp-web-manager/entrypoint.sh
+++ NCP/openncp-web-manager/entrypoint.sh
@@ -21,17 +21,6 @@ envsubst \
     < /opt/atna-resources/ArrConnections.template.xml \
     > /opt/atna-resources/ArrConnections.xml
 
-# A docker container resolves any call to localhost (or in this case 127.0.0.1) to itself instead of the network,
-# meaning the SMP discovery URL to the server will fail.
-# A common way to resolve localhost calls from inside a container to the another container is by using a HOST network instead of a BRIDGE network.
-# However this creates other problems regarding port management and so on and is not a best practice.
-# Mixing a bridge- and host network is not possible either.
-# The socat command binds the call to localhost:8443 (TCP-LISTEN:8443) to the docker host ip (host.docker.internal) to be able to access the openncp server
-# ip that comes back from the accept SMP discovery url (127.0.0.1:xxx/openncp-ws-server/xxxx)
-socat TCP-LISTEN:8443,fork TCP:host.docker.internal:8443 &
-# Same as the above but for the openatna server, since docker hostnames don't seem to work with SSL
-socat TCP-LISTEN:3443,fork TCP:host.docker.internal:3443 &
-
 # Execute the original command
 exec "$@"
 
