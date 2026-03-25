688a62bbb8d935dac7edb399e88df8e81bdadba6
Hans Bugge Grathwohl <hbg@globeteam.com>
2026-03-25 14:56:32 +0100

    make more repl-friendly

--- cert-exporter/src/dk/sundhedsdatastyrelsen/cert_exporter.clj
+++ cert-exporter/src/dk/sundhedsdatastyrelsen/cert_exporter.clj
@@ -12,7 +12,8 @@
    [java.security KeyStore]
    [java.security.cert X509Certificate]
    [java.time Instant]
-   [java.util.concurrent Executors TimeUnit]))
+   [java.util.concurrent Executors ExecutorService TimeUnit]
+   [org.eclipse.jetty.server Server]))
 
 (set! *warn-on-reflection* true)
 
@@ -52,62 +53,85 @@
 
 (defn keystore-certs
   "Returns a seq of maps with certificate metadata for every alias in the keystore."
-  [{:keys [label] :as ks-config}]
+  [{:keys [label path] :as ks-config}]
   (let [ks (load-keystore ks-config)]
     (for [alias (enumeration-seq (.aliases ks))
           :let  [cert (leaf-cert ks alias)]
           :when (instance? X509Certificate cert)]
       (assoc (cert-info cert)
              :label label
+             :path path
              :alias alias))))
 
 ;;; ---------------------------------------------------------------------------
 ;;; Prometheus metrics
 
-(def labels [:label :alias :subject :issuer])
+(defonce registry (atom {}))
 
-(defonce ^Gauge cert-not-after
-  (-> (Gauge/builder)
-      (.name "cert_not_after_timestamp_seconds")
-      (.help "Unix timestamp of certificate expiry (not-after)")
-      (.labelNames (into-array String (map name labels)))
-      (.register PrometheusRegistry/defaultRegistry)))
+(defn unregister-gauge!
+  "Remove a gauge from the default prometheus registry."
+  [g]
+  (when-some [^Gauge gauge (@registry (:name g))]
+    (-> PrometheusRegistry/defaultRegistry (.unregister gauge))))
 
-(defonce ^Gauge cert-not-before
-  (-> (Gauge/builder)
-      (.name "cert_not_before_timestamp_seconds")
-      (.help "Unix timestamp of certificate validity start (not-before)")
-      (.labelNames (into-array String (map name labels)))
-      (.register PrometheusRegistry/defaultRegistry)))
+(defn register-gauge!
+  "Register a Gauge with the default Prometheus registry. Returns the Gauge object."
+  [g]
+  ;; Perform clean-up first to make registration idempotent
+  (unregister-gauge! g)
+  (let [raw (-> (Gauge/builder)
+                (.name (:name g))
+                (.help (:help g))
+                (.labelNames (into-array String (map name (:labels g))))
+                (.register PrometheusRegistry/defaultRegistry))]
+    (swap! registry assoc (:name g) raw)
+    (assoc g :raw raw)))
+
+(defn prefix
+  "Add a prefix to the metric name of the gauge"
+  [g prefix]
+  (update g :name #(str prefix %)))
+
+(def not-after-gauge
+  {:name "not_after_timestamp_seconds"
+   :help "Unix timestamp of certificate expiry (not-after)"
+   :labels [:label :path :alias :subject :issuer]
+   :value-fn (fn [cert-data] (.getEpochSecond ^Instant (:not-after cert-data)))})
+
+(def not-before-gauge
+  {:name "not_before_timestamp_seconds"
+   :help "Unix timestamp of certificate validity start (not-before)"
+   :labels [:label :path :alias :subject :issuer]
+   :value-fn (fn [cert-data] (.getEpochSecond ^Instant (:not-before cert-data)))})
+
+(defn set-gauge!
+  "Update a gauge with a data point"
+  [gauge data]
+  (let [label-values (into-array String (map #(get data %) (:labels gauge)))
+        ^double value ((:value-fn gauge) data)]
+    (-> gauge
+        ^Gauge (:raw)
+        ^Gauge$DataPoint (.labelValues label-values)
+        (.set value))))
+
+(defn update-metrics! [keystores gauges]
+  (doseq [ks-config keystores
+          data (try
+                 (keystore-certs ks-config)
+                 (catch Exception e
+                   (log/error "failed to read keystore" (:label ks-config) ":" (.getMessage e))))
+          :when data
+          gauge gauges]
+    (set-gauge! gauge data)))
 
 (comment
-  ;; unregister gauges
-  (do (-> PrometheusRegistry/defaultRegistry (.unregister cert-not-after))
-      (-> PrometheusRegistry/defaultRegistry (.unregister cert-not-before)))
-  )
+  (def *gauges (mapv register-gauge! [not-after-gauge not-before-gauge]))
 
-(defn set-gauge! [^Gauge gauge labels data ^double val]
-  (let [label-values (into-array String (map #(get data %) labels))
-        dp ^Gauge$DataPoint (.labelValues gauge label-values)]
-    (.set dp val)))
-
-(defn set-cert-gauges! [{:keys [^Instant not-after ^Instant not-before] :as cert-data}]
-  (set-gauge! cert-not-after labels cert-data (.getEpochSecond not-after))
-  (set-gauge! cert-not-before labels cert-data (.getEpochSecond not-before)))
-
-(defn update-metrics! [keystores]
-  (doseq [ks-config keystores]
-    (try
-      (doseq [cert (keystore-certs ks-config)]
-        (set-cert-gauges! cert))
-      (catch Exception e
-        (log/warn "failed to read keystore" (:label ks-config) ":" (.getMessage e))))))
-
-(comment
   (update-metrics! [{:path "../NCP/keystore/dev-tls.jks"
                      :type "JKS"
                      :password "password"
-                     :label "NCP-dev-tls"}])
+                     :label "NCP-dev-tls"}]
+                   *gauges)
   )
 
 ;;; ---------------------------------------------------------------------------
@@ -138,33 +162,44 @@
 ;;; ---------------------------------------------------------------------------
 ;;; Scrape loop
 
-(defn start-scrape-loop! [keystores interval-seconds]
+(defn start-scrape-loop! [keystores gauges interval-seconds]
   (let [executor (Executors/newSingleThreadScheduledExecutor)]
     (.scheduleAtFixedRate executor
                           #(do (log/debug "Updating certificate metrics")
-                               (update-metrics! keystores))
-                          interval-seconds
-                          interval-seconds
+                               (update-metrics! keystores gauges))
+                          interval-seconds ; initialDelay
+                          interval-seconds ; period
                           TimeUnit/SECONDS)
     executor))
 
 ;;; ---------------------------------------------------------------------------
 ;;; Entry point
 
-(defn -main [& args]
-  (let [config-path (or (first args) "config.edn")
-        config      (load-config config-path)
-        keystores   (:keystores config)
-        port        (:port config)
-        interval    (:scrape-interval-seconds config)]
+(defn start! [config]
+  (let [keystores (:keystores config)
+        gauges (->> [not-after-gauge not-before-gauge]
+                    (mapv #(prefix % (:prefix config)))
+                    (mapv register-gauge!))
+        port (:port config)
+        interval (:scrape-interval-seconds config)]
     (log/info "Starting cert-exporter on port" port
               "with" (count keystores) "keystore(s),"
               "scrape interval" interval "s")
-    (update-metrics! keystores)
-    (start-scrape-loop! keystores interval)
-    (jetty/run-jetty handler {:port port :join? true})))
+    (update-metrics! keystores gauges)
+    {:executor (start-scrape-loop! keystores gauges interval)
+     :jetty (jetty/run-jetty handler {:port port :join? false})}))
+
+(defn stop! [{:keys [^ExecutorService executor ^Server jetty]}]
+  (.shutdown executor)
+  (.stop jetty))
+
+
+(defn -main [& args]
+  (let [config-path (or (first args) "config.edn")
+        config (load-config config-path)]
+    (start! config)))
 
 (comment
-  (-main)
-  (load-config "config.edn")
+  (def *server (start! (assoc (load-config "config.edn") :port 9091)))
+  (stop! *server)
   )
