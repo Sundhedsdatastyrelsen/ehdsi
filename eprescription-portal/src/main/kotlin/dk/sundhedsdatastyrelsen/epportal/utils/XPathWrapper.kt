0b4450b76d9d146e7a79fcb5e318f5a5fa6a4ad9
Jakob Vase <jakobvase@gmail.com>
2026-09-23 11:49:15 +0200

    add xpath wrapper from national connector to epportal

---
+++ eprescription-portal/src/main/kotlin/dk/sundhedsdatastyrelsen/epportal/utils/XPathWrapper.kt
@@ -0,0 +1,94 @@
+package dk.sundhedsdatastyrelsen.epportal.utils
+
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import javax.xml.namespace.NamespaceContext
+import javax.xml.xpath.XPath
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathFactory
+
+/**
+ * Thread-safe convenience wrapper around javax.xml.XPath.
+ *
+ * Copied and kotlinised from the similar class in national-connector. XmlException is removed, since it existed only
+ * to wrap checked exceptions.
+ */
+class XPathWrapper(namespaces: Map<String, String>) {
+    private val nsCtx: NamespaceContext = object : NamespaceContext {
+        override fun getNamespaceURI(prefix: String): String? =
+            namespaces[prefix]
+
+        override fun getPrefix(uri: String): String? =
+            namespaces.entries.find { it.value == uri }?.key
+
+        override fun getPrefixes(uri: String): Iterator<String> =
+            namespaces.entries.filter { it.value == uri }.map { it.key }.iterator()
+    }
+
+    constructor(vararg namespaces: XmlNamespace) : this(
+        namespaces.groupBy({ it.prefix!! }, { it.uri!! }).mapValues { it.value[0] }
+    )
+
+    private fun xpath(): XPath {
+        // XPathFactory and XPath are both not thread-safe, so we create a new one for each invocation.
+        // I've (HBG) benchmarked it against a more complicated optimized solution with ThreadLocals, and the
+        // difference was very small (1-2%) for the individual XPathWrapper method invocations, and immeasurable in
+        // a larger context (e.g. bootstrap token generation). So I'm going with this simple version which doesn't have
+        // space leak issues. Note: XPathFactory.newInstance() is significantly slower because it uses reflection.
+        return XPathFactory.newDefaultInstance().newXPath().apply { namespaceContext = nsCtx }
+    }
+
+    /**
+     * Locates and evaluates the content of a node to a string. Trims the string after extraction.
+     *
+     * @param expression The XPath expression linking to the node to be evaluated
+     * @param item       The XML object that needs to be traversed
+     * @return The node value as a string (using the XPath function evaluate)
+     */
+    fun evalString(expression: String, item: Any): String =
+        xpath().evaluate(expression, item).trim()
+
+    /**
+     * Locates and returns a node in an XML.
+     *
+     * @param expression The XPath expression describing the location of the node
+     * @param item       The XML object that needs to be traversed
+     * @return The node located
+     */
+    fun evalNode(expression: String, item: Any): Node? =
+        xpath().evaluate(expression, item, XPathConstants.NODE) as Node?
+
+    /**
+     * Locates and returns a node, cast as an Element, in an XML.
+     *
+     * @param expression The XPath expression describing the location of the node
+     * @param item       The XML object that needs to be traversed
+     * @return The node located
+     */
+    fun evalElement(expression: String, item: Any): Element? =
+        evalNode(expression, item) as Element?
+
+    /**
+     * Locates and return an XPath NodeSet as a List of Nodes
+     *
+     * @param expression The XPath expression describing the location of the nodes
+     * @param item       The XML object that needs to be traversed
+     * @return A NodeSet mapped to an unmodifiable list
+     */
+    fun evalNodes(expression: String, item: Any): List<Node> =
+        (xpath().evaluate(expression, item, XPathConstants.NODESET) as NodeList)
+            .let { nodeList -> (0..<nodeList.length).map { nodeList.item(it) } }
+
+    /**
+     * Locates and returns the text content values of an XPath Nodeset as a list of strings.
+     * The text content is acquired using XPath Node objects getTextContent method.
+     * The text content is trimmed.
+     *
+     * @param expression The XPath expression describing the location of the nodes
+     * @param item       The XML object that needs to be traversed
+     * @return A NodeSets text content as an unmodifiable list
+     */
+    fun evalStrings(expression: String, item: Any): List<String> =
+        evalNodes(expression, item).map { it.textContent.trim() }
+}
---
+++ eprescription-portal/src/main/kotlin/dk/sundhedsdatastyrelsen/epportal/utils/XmlNamespace.kt
@@ -0,0 +1,45 @@
+package dk.sundhedsdatastyrelsen.epportal.utils
+
+data class XmlNamespace(val prefix: String?, val uri: String?) {
+    companion object {
+        // This is a list of namespaces used in the project. Its purpose is convenience, to encourage consistency,
+        // and to avoid scattering the information around the codebase.
+        val HL7: XmlNamespace = XmlNamespace("hl7", "urn:hl7-org:v3")
+
+        /** FSK */
+        val SDTC: XmlNamespace = XmlNamespace("sdtc", "urn:hl7-org:sdtc")
+
+        /** Dispensation */
+        val PHARM: XmlNamespace = XmlNamespace("pharm", "urn:hl7-org:pharm")
+
+        /** Authentication */
+        val AUTH: XmlNamespace = XmlNamespace("auth", "http://docs.oasis-open.org/wsfed/authorization/200706")
+        val DS: XmlNamespace = XmlNamespace("ds", "http://www.w3.org/2000/09/xmldsig#")
+
+        // There are two valid DGWS/MEDCOM namespaces for historic reasons, and CPR at least requires the old one.
+        // https://svn.medcom.dk/svn/releases/Standarder/DGWS/Dokumentation/Den%20Gode%20Webservice%201.0.1.pdf:
+        // > [...] ældre webservices ikke altid har overholdt XML-skema, men anvendt det
+        // > førstnævnte namespace (MEDCOM_LEGACY). Derfor anbefales det sidstnævnte namespace
+        // > (MEDCOM) til fremtidige webservices [...]
+        val MEDCOM: XmlNamespace =
+            XmlNamespace("medcom", "http://svn.medcom.dk/svn/releases/Standarder/DGWS/Schemas/medcom-1.0.1.xsd")
+        val MEDCOM_LEGACY: XmlNamespace =
+            XmlNamespace("medcom-legacy", "http://www.medcom.dk/dgws/2006/04/dgws-1.0.xsd")
+        val SAML: XmlNamespace = XmlNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion")
+        val SOAP: XmlNamespace = XmlNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/")
+        val SOSI: XmlNamespace = XmlNamespace("sosi", "http://www.sosi.dk/sosi/2006/04/sosi-1.0.xsd")
+        val WSA: XmlNamespace = XmlNamespace("wsa", "http://www.w3.org/2005/08/addressing")
+        val WSP: XmlNamespace = XmlNamespace("wsp", "http://schemas.xmlsoap.org/ws/2004/09/policy")
+        val WSSE: XmlNamespace =
+            XmlNamespace("wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")
+        val WSSE11: XmlNamespace =
+            XmlNamespace("wsse11", "http://docs.oasis-open.org/wss/oasis-wss-wssecurity-secext-1.1.xsd")
+        val WST: XmlNamespace = XmlNamespace("wst", "http://schemas.xmlsoap.org/ws/2005/02/trust")
+        val WST13: XmlNamespace = XmlNamespace("wst13", "http://docs.oasis-open.org/ws-sx/ws-trust/200512")
+        val WST14: XmlNamespace = XmlNamespace("wst14", "http://docs.oasis-open.org/ws-sx/ws-trust/200802")
+        val WSU: XmlNamespace =
+            XmlNamespace("wsu", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd")
+        val XSD: XmlNamespace = XmlNamespace("xsd", "http://www.w3.org/2001/XMLSchema")
+        val XSI: XmlNamespace = XmlNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+    }
+}
